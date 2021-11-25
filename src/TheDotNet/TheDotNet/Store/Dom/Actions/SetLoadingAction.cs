889e0043bdeb4e5c26003465c9f7b4cf1158cc45
Hasan Hasanov <hasan.mehmedov@gmail.com>
2021-11-25 22:23:52 +0200

    Add dom state with necesseary actions

---
+++ src/TheDotNet/TheDotNet/Store/Dom/Actions/SetLoadingAction.cs
@@ -0,0 +1,12 @@
+﻿namespace TheDotNet.Store.Dom.Actions
+{
+    public record SetLoadingAction
+    {
+        public SetLoadingAction(bool isLoading)
+        {
+            IsLoading = isLoading;
+        }
+
+        public bool IsLoading { get; }
+    }
+}
---
+++ src/TheDotNet/TheDotNet/Store/Dom/Reducers/OnSetLoadingReducer.cs
@@ -0,0 +1,17 @@
+﻿using Fluxor;
+using TheDotNet.Store.Dom.Actions;
+
+namespace TheDotNet.Store.Dom.Reducers
+{
+    public class OnSetLoadingReducer : Reducer<DomState, SetLoadingAction>
+    {
+        public override DomState Reduce(DomState state, SetLoadingAction action)
+        {
+            return new DomState
+            {
+                IsLoading = action.IsLoading,
+                ErrorMessage = state.ErrorMessage,
+            };
+        }
+    }
+}
