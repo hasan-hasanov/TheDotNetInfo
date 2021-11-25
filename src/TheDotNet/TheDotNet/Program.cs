6122c7f97a89f5e814fdd103833df29d7f521a25
Hasan Hasanov <hasan.mehmedov@gmail.com>
2021-11-25 22:21:32 +0200

    Add fluxor

--- src/TheDotNet/TheDotNet/Program.cs
+++ src/TheDotNet/TheDotNet/Program.cs
@@ -1,3 +1,4 @@
+using Fluxor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TheDotNet;
@@ -7,5 +8,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddFluxor(o => o
+    .ScanAssemblies(typeof(Program).Assembly)
+    .UseReduxDevTools());
 
 await builder.Build().RunAsync();
